ea6101b77e3437cb185032c7db7304ed36bd8869
jasonStance <jason.stancil@ticketmaster.com>
2020-09-06 17:32:05 -0500

    Add files

---
+++ testfile.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_POST["Submit"])) {
+    $Username = $_POST["Username"];
+    $Password = $_POST["Password"];
+
+    if ($Username=="Jason" && $Password=="Tester") {
+        echo "<h2>Welcome : {$_POST["Username"]}</h2>";
+    }
+    else {
+        echo "<h2>Account Does't Exists | Try Again</h2>";
+
+    }
+}
+else {
+    echo "<h2>Login Required</h2>";
+}
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <title>Form Learning</title>
+
+</head>
+
+<body>
+<?php ?>
+<fieldset>
+    <legend>Test Login Form</legend>
+    <form action="testfile.php" method="POST">
+        <label for="Username">Username:</label>
+        <input id="Username" type="text" name="Username"><br><br>
+        <label for="Password">Password:</label>
+        &nbsp;
+        <input id="Password" type="Password" name="Password"><br><br>
+        <input type="Submit" name="Submit" value="Submitted">
+    </form>
+</fieldset>
+</body>
+</html>
